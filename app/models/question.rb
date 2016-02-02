800a2575c53b2e035ee580b7657c20ded927af13
aparker611 <adam3692@hotmail.co.uk>
2016-02-02 17:36:28 +0000

    Added buton to download as CSV and styled it

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,13 @@
 class Question < ActiveRecord::Base
+	def self.to_csv
+		attributes = %w{name question_one question_two question_three question_four question_six question_seven}
+
+		CSV.generate(headers: true) do |csv|
+			csv << attributes
+
+			all.each do |user|
+				csv << attributes.map{ |attr| user.send(attr) }
+			end
+		end
+	end
 end
